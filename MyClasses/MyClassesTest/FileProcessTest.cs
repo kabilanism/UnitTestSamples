7f69f4b88c6811bc306b8e57c2453dc6fc5c5bb3
Kabilan Nanthakumar <KNanthak@garda.ca>
2021-05-06 20:05:20 -0400

    Include initial test cases for file name check

---
+++ MyClasses/MyClassesTest/FileProcessTest.cs
@@ -0,0 +1,33 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void FileNameDoesExist()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void FileNameDoesNotExist()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void FileNameNullOrEmpty_UsingAttribute()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void FileNameNullOrEmpty_UsingTryCatch()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
