e50d7e65c9871fcb0029f64814c812a48201d897
Kabilan Nanthakumar <KNanthak@garda.ca>
2021-05-09 19:48:59 -0400

    Add assembly initialization class

---
+++ MyClasses/MyClassesTest/MyClassesTestInitialization.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class MyClassesTestInitialization
+    {
+        [AssemblyInitialize()] //You would include all the setup (database, files, etc. ..) for each class in this assembly to be done prior to any tests being run.
+        public static void AssemblyInitialize(TestContext tc)
+        {
+            tc.WriteLine("In AssemblyInitialize() method");
+        }
+
+        [AssemblyCleanup()] //This is where you remove anything you set up in the initialization once the tests are completed running.
+        public static void AssemblyCleanup()
+        {
+        }
+    }
+}
