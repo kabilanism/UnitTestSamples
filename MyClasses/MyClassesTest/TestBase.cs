66f0ef709f883c9c088f1cedbe5441f69d23e0fc
Kabilan Nanthakumar <KNanthak@garda.ca>
2021-05-09 19:00:40 -0400

    Include base test class to contain generic members & methods for reusability for other test classes

---
+++ MyClasses/MyClassesTest/TestBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MyClassesTest
+{
+    public class TestBase
+    {
+        public TestContext TestContext { get; set; }
+        protected string GOOD_FILE_NAME;
+        protected void SetGoodFileName()
+        {
+            GOOD_FILE_NAME = TestContext.Properties["GoodFileName"].ToString();
+            if (GOOD_FILE_NAME.Contains("[AppPath]"))
+            {
+                GOOD_FILE_NAME = GOOD_FILE_NAME.Replace("[AppPath]", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            }
+        }
+    }
+}
