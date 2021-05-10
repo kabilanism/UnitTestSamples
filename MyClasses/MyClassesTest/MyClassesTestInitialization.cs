3bc2b292be1283483ecfa56c3ddb7b54c0ef6e49
Kabilan Nanthakumar <KNanthak@garda.ca>
2021-05-09 20:26:15 -0400

    Add comments to test assembly initialization for examples on what can be added

--- MyClasses/MyClassesTest/MyClassesTestInitialization.cs
+++ MyClasses/MyClassesTest/MyClassesTestInitialization.cs
@@ -13,12 +13,21 @@ namespace MyClassesTest
         [AssemblyInitialize()] //You would include all the setup (database, files, etc. ..) for each class in this assembly to be done prior to any tests being run.
         public static void AssemblyInitialize(TestContext tc)
         {
+            //Create DB ..
+            //Create tables in the DB ..
+            //Add test data to the DB ..
+            //Create text files ..
+
             tc.WriteLine("In AssemblyInitialize() method");
         }
 
         [AssemblyCleanup()] //This is where you remove anything you set up in the initialization once the tests are completed running.
         public static void AssemblyCleanup()
         {
+            //Remove DB ..
+            //Remove tables in DB ..
+            //Remove test data ..
+            //Remove text files ..
         }
     }
 }
