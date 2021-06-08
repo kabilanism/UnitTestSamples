ef06f9cfa286f0877b0b501ba418491bab1f0c19
Kabilan Nanthakumar <KNanthak@garda.ca>
2021-06-08 13:33:58 -0400

    Include method to test base to provide description on test being executed using reflection

--- MyClasses/MyClassesTest/TestBase.cs
+++ MyClasses/MyClassesTest/TestBase.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Reflection;
 
 namespace MyClassesTest
 {
@@ -8,6 +9,25 @@ namespace MyClassesTest
         public TestContext TestContext { get; set; }
         protected string GOOD_FILE_NAME;
         protected string BAD_FILE_NAME = @"C:\Windows\Fake.exe";
+        protected void WriteDescription(Type typ)
+        {
+            string testName = TestContext.TestName;
+
+            //Find the test method currently executing
+            MemberInfo method = typ.GetMethod(testName);
+            if (method != null)
+            {
+                //See if the [Description] attribute exists on this test
+                Attribute attr = method.GetCustomAttribute(typeof(DescriptionAttribute));
+                if (attr != null)
+                {
+                    //Cast the attribute to a DescriptionAttribute
+                    DescriptionAttribute dattr = (DescriptionAttribute)attr;
+                    //Display the test description
+                    TestContext.WriteLine($"Test Description: {dattr.Description}");
+                }
+            }
+        }
         protected void SetGoodFileName()
         {
             GOOD_FILE_NAME = TestContext.Properties["GoodFileName"].ToString();
